ed0484cde17cecdaad0e02b4f68e78931719f3a9
qihaolin <936804440@qq.com>
2016-09-02 14:20:10 +0800

    够买记录优化

--- wfj-back-develop/src/main/java/com/wfj/member/controller/MemberBasicController.java
+++ wfj-back-develop/src/main/java/com/wfj/member/controller/MemberBasicController.java
@@ -1,10 +1,19 @@
 package com.wfj.member.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.constants.SystemConfig;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.wangfj.back.entity.po.SysConfig;
+import com.wangfj.back.service.ISysConfigService;
 import com.wangfj.order.utils.CommonProperties;
 import com.wangfj.order.utils.HttpUtil;
+import com.wangfj.wms.util.HttpUtilPcm;
+import com.wangfj.wms.util.JsonUtil;
+
 import net.sf.json.JSONObject;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +34,8 @@ import java.util.Map;
 @RequestMapping("/memBasic")
 public class MemberBasicController {
     private static org.slf4j.Logger log =  LoggerFactory.getLogger(MemberBasicController.class);
-
+    @Autowired
+    private ISysConfigService sysConfigService;
     /**
      * 解除黑名单
      * @param request
@@ -394,6 +404,19 @@ public class MemberBasicController {
         response.setCharacterEncoding("utf-8");
         String method = "/memBasic/getMemPurchase.do";
         String jsonString="";
+        //屏显判断
+        List<String >list = new ArrayList<String >();
+        list.add("memberInfo");
+        String sysValue="";
+        List<SysConfig> sysConfigs=null;
+		try {
+			sysConfigs = sysConfigService.selectByKeys(list);
+			sysValue = "";
+			SysConfig sysConfig = sysConfigs.get(0);
+			sysValue = sysConfig.getSysValue();
+		} catch (Exception e1) {
+			log.error("查询sysValue异常！查询结果sysConfigs="+sysConfigs+e1);
+		}
         //获取每页显示多少条数据
         Integer pageSize = 0;
         //获取当前页
@@ -404,6 +427,7 @@ public class MemberBasicController {
             pageSize = 10;
         }
         Map<String, Object> paraMap = new HashMap<String, Object>();
+        paraMap.put("sysValue", sysValue);
         paraMap.put("currPage",currPage);
         paraMap.put("pageSize",pageSize);
         paraMap.put("username",request.getParameter("cid"));
@@ -443,6 +467,19 @@ public class MemberBasicController {
         response.setCharacterEncoding("utf-8");
         String method = "/memBasic/getMemRefund.do";
         String jsonString="";
+        //屏显判断
+        List<String> syslist = new ArrayList<String>();
+        syslist.add("memberInfo");
+        String sysValue="";
+        List<SysConfig> sysConfigs=null;
+		try {
+			sysConfigs = sysConfigService.selectByKeys(syslist);
+			sysValue = "";
+			SysConfig sysConfig = sysConfigs.get(0);
+			sysValue = sysConfig.getSysValue();
+		} catch (Exception e1) {
+			log.error("查询sysValue异常！查询结果sysConfigs="+sysConfigs+e1);
+		}
         //获取每页显示多少条数据
         Integer pageSize = 0;
         //获取当前页
@@ -455,6 +492,7 @@ public class MemberBasicController {
         Map<String, Object> paraMap = new HashMap<String, Object>();
         paraMap.put("currPage",currPage);
         paraMap.put("pageSize",pageSize);
+        paraMap.put("sysValue", sysValue);
         paraMap.put("cid",request.getParameter("cid"));
         paraMap.put("mobile",request.getParameter("mobile"));
         paraMap.put("email",request.getParameter("email"));
@@ -475,4 +513,35 @@ public class MemberBasicController {
         }
         return jsonString;
     }
+ /**
+  * 查询订单来源
+  * @param request
+  * @param response
+  * @return
+  * **/
+    @ResponseBody
+    @RequestMapping(value="/selectOrderFrom",method= {RequestMethod.POST, RequestMethod.GET})
+    public String selectOrderFrom(HttpServletRequest request,HttpServletResponse response){
+    	String json = "";
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            json = HttpUtilPcm.doPost(SystemConfig.SSD_SYSTEM_URL
+                    + "/pcmAdminChannel/findListChannel.htm", JsonUtil.getJSONString(map));
+            map.clear();
+            JSONObject jsonObject = JSONObject.fromObject(json);
+            List<?> list = (List<?>) jsonObject.get("data");
+            if ((list != null) && (list.size() != 0)) {
+                map.put("list", list);
+                map.put("success", "true");
+            } else {
+                map.put("success", "false");
+            }
+        } catch (Exception e) {
+            map.put("success", "false");
+        }
+
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+        return gson.toJson(map);    	
+    }
+   
 }
