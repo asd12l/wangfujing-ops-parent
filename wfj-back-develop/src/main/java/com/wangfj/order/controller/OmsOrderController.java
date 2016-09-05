fa0baba2851a2894aa5362ad5d442dde04350ea7
chenhu <854150085@qq.com>
2016-09-05 20:04:56 +0800

    包裹单，3天内查询显示不同

--- wfj-back-develop/src/main/java/com/wangfj/order/controller/OmsOrderController.java
+++ wfj-back-develop/src/main/java/com/wangfj/order/controller/OmsOrderController.java
@@ -5248,4 +5248,81 @@ public class OmsOrderController {
 		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 		return gson.toJson(m);
 	}
+	
+	/**
+	 * 查询包裹单信息（要求有距离发货时间）
+	 * @Methods Name selectPackageInfoPage
+	 * @Create In 2016-4-14 By chenHu
+	 * @param request
+	 * @param response
+	 * @return String
+	 * @throws ParseException 
+	 */
+	@ResponseBody
+	@RequestMapping("/selectPackageInfoPage")
+	public String selectPackageInfoPage(HttpServletRequest request, HttpServletResponse response) throws ParseException {
+		String json = "";
+		Integer size = request.getParameter("pageSize")==null?null:Integer.parseInt(request.getParameter("pageSize"));
+		Integer currPage = Integer.parseInt(request.getParameter("page"));
+		if(size==null || size==0){
+			size = 10;
+		}
+		Map<Object, Object> paramMap = new HashMap<Object, Object>();
+		if(StringUtils.isNotEmpty(request.getParameter("deliveryNo"))){
+			paramMap.put("deliveryNo", request.getParameter("deliveryNo"));
+		}
+		if(StringUtils.isNotEmpty(request.getParameter("saleNo"))){
+			paramMap.put("saleNo", request.getParameter("saleNo"));
+		}
+//		paramMap.put("fromSystem", "OMSADMIN");
+		SimpleDateFormat formatter; 
+		formatter = new SimpleDateFormat ("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
+		if(StringUtils.isNotEmpty(request.getParameter("startSaleTime"))){
+			Date date = sdf.parse(request.getParameter("startSaleTime"));
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(date);
+			calendar.add(Calendar.HOUR_OF_DAY, 0);
+			Date date2 = calendar.getTime();
+			String startDate = sdf.format(date2);
+			paramMap.put("startTimeStr", startDate);
+		}
+		if(StringUtils.isNotEmpty(request.getParameter("endSaleTime"))){
+			Date date = sdf.parse(request.getParameter("endSaleTime"));
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(date);
+			calendar.add(Calendar.HOUR_OF_DAY, 0);
+			Date date2 = calendar.getTime();
+			String endDate = sdf.format(date2);
+			paramMap.put("endTimeStr", endDate);
+		}
+		Map<Object, Object> m = new HashMap<Object, Object>();
+		paramMap.put("start", String.valueOf(currPage));
+		paramMap.put("limit", String.valueOf(size));
+		try {
+			String jsonStr = JSON.toJSONString(paramMap);
+			logger.info("jsonStr:" + jsonStr);
+			json = HttpUtilPcm.doPost(CommonProperties.get("select_Package_info"),jsonStr);
+//			json = HttpUtilPcm.doPost("http://127.0.0.1:8087/oms-core-sdc/omsPackageInfo/selectNotPackagePage.htm", jsonStr);
+			JSONObject jsonObject = JSONObject.fromObject(json);
+			String data = jsonObject.getString("data");
+			JSONObject jsonObject2 = JSONObject.fromObject(data);
+			List<Object> list = (List<Object>) jsonObject2.get("list");
+			Integer count = jsonObject2.getInt("count");
+			int pageCount = count % size == 0 ? count / size : (count / size + 1);
+			if (list != null && list.size() != 0) {
+				m.put("list", list);
+				m.put("pageCount", pageCount);
+				m.put("success", "true");
+			} else {
+				m.put("pageCount", 0);
+				m.put("success", "false");
+			}
+		} catch (Exception e) {
+			m.put("success", "false");
+			m.put("success", "false");
+		}
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+		return gson.toJson(m);
+	}
 }
