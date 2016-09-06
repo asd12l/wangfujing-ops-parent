2cfa76cfe6357d3c36048ee552d7cc1566c618f3
huanghaitao <huanghaitao>
2016-09-06 17:44:16 +0800

    爱逛街js日志添加

--- wfj-back-develop/src/main/java/com/wangfj/edi/controller/EdiOrderController.java
+++ wfj-back-develop/src/main/java/com/wangfj/edi/controller/EdiOrderController.java
@@ -134,7 +134,8 @@ private static final Logger logger = LoggerFactory.getLogger(EdiOrderController.
 		}else{
 			paramMap.put("userName", "");
 		}
-		
+		String js = (String) PropertiesUtil.getContextProperty("log_js");
+		paramMap.put("logJs", js);
 		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 		System.out.println(gson.toJson(paramMap));
 		return gson.toJson(paramMap);
