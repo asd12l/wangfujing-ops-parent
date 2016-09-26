237f852a3bdd226e8f0ad0e63cf585736a97b0a1
qihaolin <936804440@qq.com>
2016-09-26 13:33:30 +0800

    cms频道校验修改

--- wfj-back-develop/src/main/webapp/js/customize/nav/channel/channel.js
+++ wfj-back-develop/src/main/webapp/js/customize/nav/channel/channel.js
@@ -217,7 +217,7 @@ function loadDir() {
 		success : function(response) {
 			var channelPath = $(".channel_path");
 			var result = response.list;
-			channelPath.html("<option value='/'> 请选择访问路径 </option>");
+			channelPath.html("<option value=''> 请选择访问路径 </option>");
 			for (var i = 0; i < result.length; i++) {
 				var ele = result[i];
 				if (ele.id == "") continue;
@@ -288,6 +288,7 @@ function tab(data) {
  */
 function addChannel() {
 	$("#addChannelForm")[0].reset();
+	$(".error").html("");
 	// --Fuelux Spinner--
 	$('.spinner').spinner('value', 1);
 	$("#addChannelDIV").show();
